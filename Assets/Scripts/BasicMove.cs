efda5656c5801d18d37cd57b288455da95d358e9
Joey Liu <46060748+JoeyLiuTsu@users.noreply.github.com>
2021-02-02 23:10:22 -0800

    Added Test Scene
    -Added a test scene
    -deleted main scene
    -test scene has various object to base your greyblocks off of.

---
+++ Assets/Scripts/BasicMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMove : MonoBehaviour
+{
+    public float walkSpeed;
+    private Rigidbody2D rb2D;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float directionX = Input.GetAxisRaw("Horizontal");
+        float directionY = Input.GetAxisRaw("Vertical");
+        Vector3 moveAmountX = Vector3.right * walkSpeed * directionX * Time.deltaTime;
+        Vector3 moveAmountY = Vector3.up * walkSpeed * directionY * Time.deltaTime;
+        rb2D.transform.position += moveAmountX + moveAmountY;
+    }
+}
